87828e306b07bdd4cf364a5cbcff9dab10719d3f
henok256 <henokmengesha91@gmail.com>
2021-04-20 15:06:50 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Form from './Form'
+
+function App() {
+  return (
+    <div className="App">
+    <h2>Write to us</h2>
+    <Form />
+    </div>
+    
+  );
+}
+
+export default App;
